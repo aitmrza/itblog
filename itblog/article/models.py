06a534594e4cbdce520674e8738872969de9491b
0muraliev <0muraliev@github.com>
2020-06-04 20:10:37 +0300

    add Author

--- itblog/article/models.py
+++ itblog/article/models.py
@@ -5,4 +5,16 @@ class Article(models.Model):
     text = models.TextField()
     likes = models.IntegerField(default=0)
     active = models.BooleanField(default=True)
+    author = models.ForeignKey(
+        to='Author', on_delete=models.CASCADE,
+        related_name="articles", null=True, blank=True
+    )
+
+    def __str__(self):
+        return self.title
     
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
--- itblog/article/views.py
+++ itblog/article/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from .models import Article
+from .models import Article, Author
 
 
 def homepage(request):
     articles = Article.objects.all()
+    wind = Author.objects.get(id=1)
 
-    return render(request, "article/homepage.html", {"articles": articles})
+    return render(request, "article/homepage.html", 
+        {   
+            "articles": articles,
+            "wind": wind
+        })
