d3a939db8abe3f653df8c55c45cc4923f58b127b
Aitmyrza Omuraliev <omuraliev2017@bk.ru>
2020-12-27 20:02:34 +0600

    Add migration and change path to Databases.

--- itblog/settings.py
+++ itblog/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 import dj_database_url
@@ -64,7 +65,7 @@ WSGI_APPLICATION = 'itblog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
     }
 }
 
